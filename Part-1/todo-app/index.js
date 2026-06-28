38880074c014062f99493837476d2208adef7320
kevin1-john1 <kevinjohn971@outlook.com>
2026-06-28 12:06:42 +0530

    Exercise 1.5 make todo app respond to GET /

--- Part-1/todo-app/index.js
+++ Part-1/todo-app/index.js
@@ -2,9 +2,25 @@ const http = require("http");
 
 const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((_req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Todo app\n");
+const server = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`
+      <html>
+        <head>
+          <title>Todo App</title>
+        </head>
+        <body>
+          <h1>Todo App</h1>
+          <p>Hello from the DevOps with Kubernetes todo app!</p>
+        </body>
+      </html>
+    `);
+    return;
+  }
+
+  res.writeHead(404, { "Content-Type": "text/plain" });
+  res.end("Not found\n");
 });
 
 server.listen(PORT, () => {
